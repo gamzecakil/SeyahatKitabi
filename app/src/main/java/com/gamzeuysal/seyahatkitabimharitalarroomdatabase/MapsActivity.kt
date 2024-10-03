6957faf9a82babee26433808df7be209a89f91e6
gamzecakil <gmzcakil@gmail.com>
2024-10-03 18:36:48 +0300

    harita üzerindeki varsayılan konum degistirildi.

--- app/src/main/java/com/gamzeuysal/seyahatkitabimharitalarroomdatabase/MapsActivity.kt
+++ app/src/main/java/com/gamzeuysal/seyahatkitabimharitalarroomdatabase/MapsActivity.kt
@@ -33,8 +33,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val kartalBridge = LatLng(40.90688808788227, 29.210851480028133)
+        mMap.addMarker(MarkerOptions().position(kartalBridge).title("Kartal Köprüsü"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(kartalBridge,15f))
     }
 }
