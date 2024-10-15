ad3a48804fcaea4c9b1b0df031e57d5c7e5b5088
gamzecakil <gmzcakil@gmail.com>
2024-10-15 12:49:57 +0300

    model yazıldı

---
+++ app/src/main/java/com/gamzeuysal/seyahatkitabimharitalarroomdatabase/model/Place.kt
@@ -0,0 +1,17 @@
+package com.gamzeuysal.seyahatkitabimharitalarroomdatabase.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Place(
+    @ColumnInfo(name="name")
+    var name : String,
+    @ColumnInfo(name="latitude")
+    var latitude :Double,
+    @ColumnInfo(name="longitude")
+    var longitude : Double) {
+    @PrimaryKey(autoGenerate = true)
+    val uid :Int = 0
+}
